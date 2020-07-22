3b85f0a9cfe2ffc95520422e5e3786992900b96e
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-07-23 00:31:16 +0200

    add the players inputs, update the UI and add some conditions in comments

--- bin/main.rb
+++ bin/main.rb
@@ -2,11 +2,16 @@
 
 puts "Welcome to Tic Tac Toe"
 puts "Please Enter Your Name(First Player): "
-
-#after entering the name we will agign the sign to player
+player1 = gets.chomp
+# save the name in player1 variable
+# after entering the name we will agign the sign to player
+puts "#{player1} your symbol is X "
 puts "Please Enter Your Name(Second Player): "
+player2 = gets.chomp
+# save the name in player2 variable
+# after entering the name we will assign the sign to player
+puts "#{player2} your symbol is O "
 
-#after entering the name we will agign the sign to player
 puts "-------------"
 puts "| a | b | c |"
 puts "-------------"
@@ -17,10 +22,15 @@ puts "-------------"
 
 puts "It is your move #{player1}, Select the position"
 # we will save the position and sign
-# we will check the winner
+# we will update the board and display it
+# we will check the conditions
 
 puts "It is your move #{player2}, Select the position"
 # we will save the position and sign
-# we will check the winner
+# we will update the board and display it
+# we will check the conditions
 
-
+puts "player 'e.g player1' is the winner! or the game is draw"
+puts "do you want to rematch(y/n)? "
+rematch = gets.chomp
+# proceed depending on the user input either to terminate the game or rematch 
