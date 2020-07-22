4289dee8e2c1045da71866c7db1a70372e2d392b
umairarshadbutt <umair6622@gmail.com>
2020-07-23 03:03:57 +0500

    Design the board view and interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+
+puts "Welcome to Tic Tac Toe"
+puts "Please Enter Your Name(First Player): "
+
+#after entering the name we will agign the sign to player
+puts "Please Enter Your Name(Second Player): "
+
+#after entering the name we will agign the sign to player
+puts "-------------"
+puts "| a | b | c |"
+puts "-------------"
+puts "| d | e | f |"
+puts "-------------"
+puts "| g | h | i |"
+puts "-------------"
+
+puts "It is your move #{player1}, Select the position"
+# we will save the position and sign
+# we will check the winner
+
+puts "It is your move #{player2}, Select the position"
+# we will save the position and sign
+# we will check the winner
+
+
