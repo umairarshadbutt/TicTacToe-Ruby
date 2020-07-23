ddb5cf89ef4da2587a5b0d34587479a06f720604
umairarshadbutt <umair6622@gmail.com>
2020-07-23 21:27:22 +0500

    Clean code, arrange the methods

--- bin/main.rb
+++ bin/main.rb
@@ -6,14 +6,9 @@ game = GameLogic.new
 puts 'Welcome to Tic Tac Toe'
 puts 'Please Enter Your Name(First Player): '
 player1 = Players.new(gets.chomp, 'X')
-# save the name in player1 variable
-# after entering the name we will agign the sign to player
 game.welcome_message(player1.name, player1.symbol)
-# puts "#{player1} your symbol is X "
 puts 'Please Enter Your Name(Second Player): '
 player2 = Players.new(gets.chomp, 'O')
-# save the name in player2 variable
-# after entering the name we will assign the sign to player
 game.welcome_message(player2.name, player2.symbol)
 test = false
 while test == false
@@ -45,17 +40,6 @@ while test == false
     break if update_game_board == true
   end
 end
-
-# get the input
-# check the validaity of input => pass the input in a method
-# we will save the position and sign
-# we will update the board and display it
-# we will check the conditions
-
-puts "It is your move #{player2.name}, Select the position"
-# we will save the position and sign
-# we will update the board and display it
-# we will check the conditions
 
 puts "player 'e.g player1' is the winner! or the game is draw"
 puts 'do you want to rematch(y/n)? '
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,6 +5,20 @@ class GameLogic
     @array = array
   end
 
+  def welcome_message(name, symbol)
+    puts "You are  welcome #{name}, Your symbol is #{symbol}"
+  end
+
+  def game_board()
+    puts '-------------'
+    puts "| #{@array[0]} | #{@array[1]} | #{@array[2]} |"
+    puts '-------------'
+    puts "| #{@array[3]} | #{@array[4]} | #{@array[5]} |"
+    puts '-------------'
+    puts "| #{@array[6]} | #{@array[7]} | #{@array[8]} |"
+    puts '-------------'
+  end
+
   def validate_move(move)
     @arrays_key = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7, 'i' => 8 }
     @arrays_key.key?(move) ? @arrays_key[move] : 'No Valid'
@@ -18,19 +32,5 @@ class GameLogic
       puts 'Move is Invalid'
       false
     end
-  end
-
-  def welcome_message(name, symbol)
-    puts "You are  welcome #{name}, Your symbol is #{symbol}"
-  end
-
-  def game_board()
-    puts '-------------'
-    puts "| #{@array[0]} | #{@array[1]} | #{@array[2]} |"
-    puts '-------------'
-    puts "| #{@array[3]} | #{@array[4]} | #{@array[5]} |"
-    puts '-------------'
-    puts "| #{@array[6]} | #{@array[7]} | #{@array[8]} |"
-    puts '-------------'
   end
 end
