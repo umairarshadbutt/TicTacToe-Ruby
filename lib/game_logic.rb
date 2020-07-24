01425cb3892ce4206f3d64a762d8ad6ecf2221c9
umairarshadbutt <umair6622@gmail.com>
2020-07-24 20:26:24 +0500

    Add a single line

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -23,6 +23,7 @@ class GameLogic
     end
   end
 
+  
   def winning?
     @win_cases = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     @win_cases.length.times do |element|
