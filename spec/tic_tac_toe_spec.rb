829cdf4e0be9925e5f330fa8d6c63458a147c480
umairarshadbutt <umair6622@gmail.com>
2020-07-28 19:38:10 +0500

    game winning condition for  false is added

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/game_logic.rb'
 require_relative '../lib/players.rb'
 describe 'Tictactoe' do
   game = GameLogic.new
+  game1 = GameLogic.new
   player1 = Players.new('Elias', 'X')
   player2 = Players.new('Umair', 'O')
   describe 'validate move' do
@@ -33,21 +34,22 @@ describe 'Tictactoe' do
       expect(game.winner(player1, player2)).to eql('Elias')
     end
   end
+  game.array = %w[a b c d e f g h i]
   describe 'update_board' do
     game.array = %w[a b c d e f g h i]
     it 'Assigns the O to board position a' do
-      expect(game.update_board(3, 'O')).to eql(true)
+      expect(game1.update_board(3, 'O')).to eql(true)
     end
     it 'Assigns the 0 to board position b' do
-      expect(game.update_board(4, 'X')).to eql(true)
+      expect(game1.update_board(4, 'X')).to eql(true)
     end
     it 'Assigns the 0 to board position c' do
-      expect(game.update_board(5, 'O')).to eql(true)
+      expect(game1.update_board(5, 'O')).to eql(true)
     end
   end
   describe 'winning?' do
-    it 'Game wining condition is true' do
-      expect(game.winning?).to eql(true)
+    it 'Game wining condition is false' do
+      expect(game1.winning?).to eql(false)
     end
   end
   describe 'winner' do
